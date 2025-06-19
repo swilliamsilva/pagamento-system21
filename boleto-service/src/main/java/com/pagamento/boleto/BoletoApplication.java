e6bc2e5a3c7193042bd3910ff5089d8c557862f6
William Silva <williamsilva.codigo@gmail.com>
2025-06-19 03:19:34 -0300

    [#09] Add Boleto Application class

---
+++ boleto-service/src/main/java/com/pagamento/boleto/BoletoApplication.java
@@ -0,0 +1,11 @@
+package com.pagamento.boleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoletoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BoletoApplication.class, args);
+    }
+}
