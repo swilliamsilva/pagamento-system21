855c0d29429cac77ebafeba53ff069f66115681c
William Silva <williamsilva.codigo@gmail.com>
2025-06-19 03:19:26 -0300

    [#02] Add API Gateway structure

---
+++ api-gateway/src/main/java/com/pagamento/gateway/config/SwaggerConfig.java
@@ -0,0 +1,8 @@
+package com.pagamento.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    // TODO: Add configuration beans
+}
