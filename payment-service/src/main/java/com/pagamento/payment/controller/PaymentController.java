4170f764b617a910db2bd0c2e0409e822a78f48a
William Silva <williamsilva.codigo@gmail.com>
2025-06-19 03:19:29 -0300

    [#04] Add Payment Orchestrator structure

---
+++ payment-service/src/main/java/com/pagamento/payment/controller/PaymentController.java
@@ -0,0 +1,10 @@
+package com.pagamento.payment.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping("/api/payment")
+public class PaymentController {
+    // TODO: Implement controller endpoints
+}
---
+++ payment-service/src/main/java/com/pagamento/payment/service/PaymentService.java
@@ -0,0 +1,8 @@
+package com.pagamento.payment.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+    // TODO: Implement business logic
+}
