61e9a2376718fdc9fb4d6fa8948b35ef6848d311
William Silva <williamsilva.codigo@gmail.com>
2025-06-19 03:19:26 -0300

    [#03] Add Auth Service structure

---
+++ auth-service/src/main/java/com/pagamento/auth/controller/AuthController.java
@@ -0,0 +1,10 @@
+package com.pagamento.auth.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    // TODO: Implement controller endpoints
+}
---
+++ auth-service/src/main/java/com/pagamento/auth/service/AuthService.java
@@ -0,0 +1,8 @@
+package com.pagamento.auth.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    // TODO: Implement business logic
+}
