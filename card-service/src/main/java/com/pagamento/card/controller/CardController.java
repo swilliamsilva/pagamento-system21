46c29c2d554d3823c4a27eacf4348b9f8d3ae409
William Silva <williamsilva.codigo@gmail.com>
2025-06-19 03:19:36 -0300

    [#11] Add Card Service structure

---
+++ card-service/src/main/java/com/pagamento/card/controller/CardController.java
@@ -0,0 +1,10 @@
+package com.pagamento.card.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping("/api/card")
+public class CardController {
+    // TODO: Implement controller endpoints
+}
---
+++ card-service/src/main/java/com/pagamento/card/service/CardService.java
@@ -0,0 +1,8 @@
+package com.pagamento.card.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CardService {
+    // TODO: Implement business logic
+}
