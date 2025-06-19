d63776e7708556f55dff91254046bd0877b5c530
William Silva <williamsilva.codigo@gmail.com>
2025-06-19 03:19:38 -0300

    [#14] Add security configuration

---
+++ auth-service/src/main/java/com/pagamento/auth/security/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.pagamento.auth.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+    // TODO: Add configuration beans
+}
