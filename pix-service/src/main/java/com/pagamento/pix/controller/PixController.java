3a97856cabf6b2a1ef1bafe4e31fa88cb920c5b7
William Silva <williamsilva.codigo@gmail.com>
2025-06-19 03:19:35 -0300

    [#10] Add Pix Service structure

---
+++ pix-service/src/main/java/com/pagamento/pix/controller/PixController.java
@@ -0,0 +1,10 @@
+package com.pagamento.pix.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping("/api/pix")
+public class PixController {
+    // TODO: Implement controller endpoints
+}
---
+++ pix-service/src/main/java/com/pagamento/pix/service/PixService.java
@@ -0,0 +1,8 @@
+package com.pagamento.pix.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PixService {
+    // TODO: Implement business logic
+}
